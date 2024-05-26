2fbe3361fe180b57720d1be03011eb88a1c0bb8c
Jamille <jamille.araujo.freitas2003@gmail.com>
2024-05-26 14:07:22 -0300

    first commit

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const routes = express.Router();
+
+const users = [{
+  id: 1,
+  name: "Jamille",
+  email: "jamille@gmail.com",
+  password: "123456"
+}]
+
+routes.post('/login', (req, res) => {
+  const {email, password } = req.body;
+  const user = users.find(user => user.email === email && user.password === password);
+  
+  if(user) {
+    res.status(200).json(user);
+  } else {
+    res.status(401).json({ message: "Credenciais invalidas" });
+  }
+});
+
+module.exports = routes;
